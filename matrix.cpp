6aa11e281688c36e0ac178f3d3f88fdbe94da7fc
Artemis <82722644+Artya7x@users.noreply.github.com>
2022-02-25 16:39:11 +0200

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -187,7 +187,7 @@ void matrix::inverse(){
 			array3[1][1]=(array[0][0]*array[2][2])-(array[0][2]*array[2][0]);
 			array3[2][1]=-((array[0][0]*array[2][1])-(array[0][1]*array[2][0]));
 			
-			array3[0][2]=(array[1][1]*array[0][2])-(array[1][2]*array[0][1]);
+			array3[0][2]=(array[0][1]*array[1][2])-(array[0][2]*array[1][1]);
 			array3[1][2]=-((array[0][0]*array[1][2])-(array[0][2]*array[1][0]));
 			array3[2][2]=(array[0][0]*array[1][1])-(array[1][0]*array[0][1]);
 			
